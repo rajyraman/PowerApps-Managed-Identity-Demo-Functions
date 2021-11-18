37d1e254bd5e7c29b84e9949cb876e6ec1481871
Natraj Yegnaraman <natraj@dreamingincrm.com>
2021-11-18 22:00:11 +1100

    Add WhoAmI

--- src/Functions/PowerAppsFunction.cs
+++ src/Functions/PowerAppsFunction.cs
@@ -22,12 +22,12 @@ using Microsoft.Xrm.Sdk.Metadata;
 
 namespace PowerAppsManagedIdentityDemoFunctions.Functions
 {
-    public class WhoAmIFunction
+    public class PowerAppsFunction
     {
         private readonly ServiceClient _serviceClient;
         private readonly HttpClient _client;
         private readonly FunctionSettings _settings;
-        public WhoAmIFunction(IOrganizationService serviceClient, IHttpClientFactory httpClientFactory, IOptions<FunctionSettings> options)
+        public PowerAppsFunction(IOrganizationService serviceClient, IHttpClientFactory httpClientFactory, IOptions<FunctionSettings> options)
         {
             _serviceClient = serviceClient as ServiceClient;
             _client = httpClientFactory.CreateClient();
@@ -39,7 +39,7 @@ namespace PowerAppsManagedIdentityDemoFunctions.Functions
         [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
         [OpenApiParameter(name: "entityName", In = ParameterLocation.Path, Required = true, Type = typeof(string), Description = "The entity to retrieve metadata for")]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/json", bodyType: typeof(string), Description = "Response with entity metadata")]
-        public ActionResult WhoAmI(
+        public ActionResult EntityMetadata(
             [HttpTrigger(AuthorizationLevel.Function, "get", Route = "entity/{entityName}")] HttpRequest req,
             string entityName,
             ILogger log)
@@ -55,7 +55,7 @@ namespace PowerAppsManagedIdentityDemoFunctions.Functions
         [OpenApiOperation(operationId: "whoami", tags: "PowerApps", Description = "Get details about current Managed Identity user", Summary = "Get details about current Managed Identity user using raw HTTP")]
         [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/json", bodyType: typeof(string), Description = "Response with userId, organization and business unit")]
-        public async Task<ActionResult> Token(
+        public async Task<ActionResult> WhoAmI(
             [HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req,
             ILogger log)
         {
@@ -67,7 +67,35 @@ namespace PowerAppsManagedIdentityDemoFunctions.Functions
             var whoAmIResponse = await (await _client.GetAsync($"{_settings.EnvironmentUrl}/api/data/v9.2/WhoAmI()")).Content.ReadAsStringAsync();
             dynamic data = JsonConvert.DeserializeObject(whoAmIResponse);
             return new OkObjectResult(data);
-
         }
+
+        [FunctionName("fetchxml")]
+        [OpenApiOperation(operationId: "fetchxml", tags: "PowerApps", Description = "Execute FetchXML query", Summary = "Execute FetchXML query")]
+        [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
+        [OpenApiRequestBody(contentType: "application/json", bodyType: typeof(FetchXMLRequest), Required = true, Description = "FetchXML query to execute")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/json", bodyType: typeof(string), Summary = "Response with entity records")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.BadRequest, contentType: "text/json", bodyType: typeof(string), Summary = "Invalid FetchXML")]
+        public async Task<ActionResult> FetchXML(
+            [HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation($"C# HTTP trigger function processed FetchXML request.");
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var request = JsonConvert.DeserializeObject<FetchXMLRequest>(requestBody);
+            if (string.IsNullOrEmpty(request.FetchXML)) return new BadRequestObjectResult(new InvalidRequest { Reason = "FetchXML is required" });
+
+            var entities = _serviceClient.GetEntityDataByFetchSearch(request.FetchXML);
+            return new OkObjectResult(entities);
+        }
+    }
+
+    public class FetchXMLRequest
+    {
+        public string FetchXML { get; set; }
+    }
+
+    public class InvalidRequest 
+    {
+        public string Reason { get; set; }
     }
 }
