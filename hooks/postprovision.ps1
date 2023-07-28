32fee1c1c1386dbe1d51c53285651024eba6aaa1
Natraj Yegnaraman <natraj@dreamingincrm.com>
2023-07-28 18:13:56 +1000

    update

--- hooks/postprovision.ps1
+++ hooks/postprovision.ps1
@@ -15,7 +15,7 @@ if ($null -ne $env:AZURE_CLIENT_ID) {
   Invoke-WebRequest -Headers $headers "https://api.bap.microsoft.com/providers/Microsoft.BusinessAppPlatform/adminApplications/$($env:AZURE_CLIENT_ID)?api-version=2020-10-01" -Method Put
   $environmentId = (pac org who --json | ConvertFrom-Json).EnvironmentId;
   $body = ConvertTo-Json -InputObject @{servicePrincipalAppId = $functionAppId }
-  Write-Host "`n‼ Creating Function App ""$env:FUNCTIONS_NAME"" with AppId ""$functionAppId"" as an user in $env:DATAVERSE_URL ‼" -ForegroundColor Yellow;
+  Write-Host "`n‼ Creating Function App ""$env:FUNCTIONS_NAME"" with AppId ""$functionAppId"" as an application user in $env:DATAVERSE_URL ‼" -ForegroundColor Yellow;
   Write-Output "Invoke POST on https://api.bap.microsoft.com/providers/Microsoft.BusinessAppPlatform/scopes/admin/environments/$($environmentId)/addAppUser?api-version=2020-10-01"
   Invoke-WebRequest -Headers $headers https://api.bap.microsoft.com/providers/Microsoft.BusinessAppPlatform/scopes/admin/environments/$($environmentId)/addAppUser?api-version=2020-10-01 -Method Post -Body $body -ContentType "application/json"
 }
