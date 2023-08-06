a5f389183f39b4f194f2d3c224cd484390c5f3a4
Natraj Yegnaraman <natraj@dreamingincrm.com>
2023-08-06 12:29:56 +1000

    update

--- hooks/postdeploy.ps1
+++ hooks/postdeploy.ps1
@@ -1,9 +1,1 @@
-azd env select $env:AZURE_ENV_NAME
-#Credit: https://github.com/Azure/azure-dev/issues/1697#issue-1617610507
-$output = azd env get-values
-foreach ($line in $output) {
-  $name, $value = $line.Split("=")
-  $value = $value -replace '^\"|\"$'
-  [Environment]::SetEnvironmentVariable($name, $value)
-}
 Write-Host "`n‼ Functions code deployed. Swagger URL App https://$env:FUNCTIONS_NAME.azurewebsites.net/api/swagger/ui ‼" -ForegroundColor Yellow;
--- hooks/postprovision.ps1
+++ hooks/postprovision.ps1
@@ -6,17 +6,22 @@ foreach ($line in $output) {
   [Environment]::SetEnvironmentVariable($name, $value)
 }
 $functionAppId = az ad sp list --filter "displayName eq '$env:FUNCTIONS_NAME'" --query '[].appId' --output tsv;
-if ($null -ne $env:AZURE_CLIENT_ID) {
+if ($null -ne $env:CI) {
+  # Set FUNCTIONS_NAME as a variable on the environment
+  gh variable set FUNCTIONS_NAME --env $env:GITHUB_ENV_NAME --body $env:FUNCTIONS_NAME
+
   $token = az account get-access-token --resource=https://api.bap.microsoft.com/ --query accessToken --output tsv
   $headers = @{
     Authorization = "Bearer $token"
   };
   Write-Output "Invoke PUT on https://api.bap.microsoft.com/providers/Microsoft.BusinessAppPlatform/adminApplications/$($env:AZURE_CLIENT_ID)?api-version=2020-10-01"
+  # Give Application permission to create Service Principal
   Invoke-WebRequest -Headers $headers "https://api.bap.microsoft.com/providers/Microsoft.BusinessAppPlatform/adminApplications/$($env:AZURE_CLIENT_ID)?api-version=2020-10-01" -Method Put
   $environmentId = (pac org who --json | ConvertFrom-Json).EnvironmentId;
   $body = ConvertTo-Json -InputObject @{servicePrincipalAppId = $functionAppId }
   Write-Host "`n‼ Creating Function App ""$env:FUNCTIONS_NAME"" with AppId ""$functionAppId"" as an application user in $env:DATAVERSE_URL ‼" -ForegroundColor Yellow;
   Write-Output "Invoke POST on https://api.bap.microsoft.com/providers/Microsoft.BusinessAppPlatform/scopes/admin/environments/$($environmentId)/addAppUser?api-version=2020-10-01"
+  # Create Function App as a Service Principal with System Admin role (implicit) in the environment
   Invoke-WebRequest -Headers $headers https://api.bap.microsoft.com/providers/Microsoft.BusinessAppPlatform/scopes/admin/environments/$($environmentId)/addAppUser?api-version=2020-10-01 -Method Post -Body $body -ContentType "application/json"
 }
 else {
