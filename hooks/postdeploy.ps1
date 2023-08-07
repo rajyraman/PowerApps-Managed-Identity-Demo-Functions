8b797e4a95dada8df8f3b03b4af56966736ba059
Natraj Yegnaraman <natraj@dreamingincrm.com>
2023-08-08 06:50:47 +1000

    Add env refresh

--- hooks/postdeploy.ps1
+++ hooks/postdeploy.ps1
@@ -1,1 +1,6 @@
-Write-Host "`n‼ Functions code deployed. Swagger URL is https://$env:FUNCTIONS_NAME.azurewebsites.net/api/swagger/ui ‼" -ForegroundColor Yellow;
+if ($null -ne $env:CI) {
+    azd env refresh
+}
+else {
+    Write-Host "`n‼ Functions code deployed. Swagger URL is https://$env:FUNCTIONS_NAME.azurewebsites.net/api/swagger/ui ‼" -ForegroundColor Yellow;
+}
